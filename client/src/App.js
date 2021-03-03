3981c2c932d713fb16f316baf27b5d6054464762
flyarem <flyare@gmail.com>
2021-03-03 16:58:09 +0700

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import React, { Fragment } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import "./App.css";
+
 const App = () => {
     return (
-        <Fragment>
-            <Nav />
-            <Landing />
-        </Fragment>
+        <Router>
+            <Fragment>
+                <Nav />
+                <Route part="/">
+                    <Landing />
+                </Route>
+                <section className="container">
+                    <Switch>
+                        <Route exact part="/register" component={Register} />
+                        <Route exact part="/login" component={Login} />
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
     );
 };
 
