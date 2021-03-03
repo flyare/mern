7658caa1f7fbb09565a4be1617e1770b7139bfbb
flyarem <flyare@gmail.com>
2021-03-03 13:55:51 +0700

    done post like

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -131,4 +131,39 @@ route.put("/like/:id", auth, async (req, res) => {
     }
 });
 
+// @route   PUT  api/posts/unlikes/id
+// @dest    Unlike a post
+// @access  Private
+route.put("/unlike/:id", auth, async (req, res) => {
+    console.log(1);
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: "Post not found." });
+        }
+
+        if (
+            post.likes.filter((likes) => likes.user.toString() === req.user.id)
+                .length === 0
+        ) {
+            return res.status(400).json({ msg: "Post not yet been liked." });
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === "ObjectId") {
+            return res.status(404).json({ msg: 'Post not found.' });
+        }
+        return res.status(500).json({ msg: "Server Error." });
+    }
+});
+
 module.exports = route;
