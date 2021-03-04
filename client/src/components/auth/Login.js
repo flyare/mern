155c9a05b6cad49abacc89b2433960339b2e6bb0
flyarem <flyare@gmail.com>
2021-03-04 10:53:18 +0700

    and form register and login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,39 @@
+import { Link } from "react-router-dom";
 const Login = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="large text-primary">Login</h1>            
+            <form className="form">
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                    />
+                    <small className="form-text">
+                        This site uses Gravatar so if you want a profile image,
+                        use a Gravatar email
+                    </small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength={6}
+                    />
+                </div>                
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    defaultValue="Register"
+                />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/register">Sign In</Link>
+            </p>
+        </div>
+    );
 };
 
 export default Login;
