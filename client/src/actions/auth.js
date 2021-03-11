ffcb4a2da1ba521f8b4f1bf40e83c3ce50fad293
flyarem <flyare@gmail.com>
2021-03-11 10:46:26 +0700

    login done

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,11 @@
 import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
 } from "../actions/types";
 import axios from "axios";
 import { setAlert } from "./alert";
@@ -12,82 +13,84 @@ import { setAuthToken } from "../utils/setAuthToken";
 
 // Loaded user
 export const loadUser = () => async (dispatch) => {
-  if (localStorage.token) {
-    setAuthToken(localStorage.token);
-  }
+    if (localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
 
-  try {
-    const res = await axios.get("/api/auth");
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR,
-    });
-  }
+    try {
+        const res = await axios.get("/api/auth");
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR,
+        });
+    }
 };
 
 // Register user
 export const register = ({ name, email, password }) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  const body = JSON.stringify({ name, email, password });
+    const body = JSON.stringify({ name, email, password });
 
-  try {
-    const res = await axios.post("/api/users", body, config);
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data,
-    });
+    try {
+        const res = await axios.post("/api/users", body, config);
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data,
+        });
 
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
-    if (errors) {
-      errors.forEach((e) => dispatch(setAlert(e.msg, "danger")));
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if (errors) {
+            errors.forEach((e) => dispatch(setAlert(e.msg, "danger")));
+        }
+
+        dispatch({
+            type: REGISTER_FAIL,
+        });
     }
-
-    dispatch({
-      type: REGISTER_FAIL,
-    });
-  }
 };
 
 // Login user
 export const login = (email, password) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  const body = JSON.stringify({ email, password });
+    const body = JSON.stringify({ email, password });
 
-  try {
-    const res = await axios.post("/api/auth", body, config);
+    try {
+        const res = await axios.post("/api/auth", body, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data,
+        });
+        dispatch(loadUser());
+    } catch (err) {
+        if (err.message) {
+            dispatch(setAlert(err.message, "danger"));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL,
+        });
+    }
+};
+
+// Logout /Clear profile
+export const logout = () => (dispatch) => {
     dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data,
+        type: LOGOUT,
     });
-    dispatch(loadUser());
-  } catch (err) {
-      if (err.message) {
-          dispatch(setAlert(err.message, "danger"));
-      }
-    // console.log(JSON.stringify(err));
-    // const errors = err.response.data.errors;
-    // if (errors) {
-    //   errors.forEach((e) => dispatch(setAlert(e.msg, "danger")));
-    // }
-
-    dispatch({
-      type: LOGIN_FAIL,
-    });
-  }
 };
--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -1,6 +1,34 @@
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
 
-const Nav = () => {
+const Nav = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        <ul>
+            <li>
+                <a href="#!" onClick={logout}>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    );
+
+    const guestLinks = (
+        <ul>
+            <li>
+                <Link to="!#">Developers</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+        </ul>
+    );
+
     return (
         <nav className="navbar bg-dark">
             <h1>
@@ -8,19 +36,20 @@ const Nav = () => {
                     <i className="fas fa-code" /> DevConnector
                 </Link>
             </h1>
-            <ul>
-                <li>
-                    <Link to="!#">Developers</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
+            {!loading && (
+                <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+            )}
         </nav>
     );
 };
 
-export default Nav;
+Nav.propTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Nav);
